9d44790f02dfa210f288d02dc9676e8cb779d22b
msaaddev <mrsaadirfan@gmail.com>
2022-12-27 21:45:54 +0500

    📦 NEW: Creating Next project

--- utils/mdx/index.js
+++ utils/mdx/index.js
@@ -1,7 +1,24 @@
-const question = require('../question');
 const { command } = require('execa');
 const ora = require('ora');
+const handleError = require('node-cli-handle-error');
+const question = require('../question');
+const { start, succeed, fail } = require('../../functions/spinner');
 
 module.exports = async () => {
 	const { projectName, projectLang } = await question();
+	const spinner = ora();
+	console.log();
+	try {
+		start(spinner, 'Creating Next project...');
+		if (projectLang === 'JavaScript') {
+			await command(`npx create-next-app ${projectName}`);
+		} else {
+			await command(`npx create-next-app ${projectName} --typescript`);
+		}
+		succeed(spinner, 'Next project created successfully');
+	} catch (err) {
+		console.log();
+		fail(spinner, err);
+		handleError(err);
+	}
 };

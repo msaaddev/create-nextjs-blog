91aa765125ab23d2e50cedf3dd74f5e6809b5284
msaaddev <mrsaadirfan@gmail.com>
2022-12-28 00:44:18 +0500

    📦 NEW: Scafold TypeScript files

---
+++ utils/md/typescript/index.js
@@ -0,0 +1,81 @@
+// external packages
+const copy = require('copy-template-dir');
+
+// built-in Node.js modules
+const cwd = process.cwd();
+const path = require('path');
+
+module.exports = (projectName = '') => {
+	let source, destination;
+	// copy blog page
+	source = path.join(
+		__dirname,
+		'..',
+		'..',
+		'..',
+		'template',
+		'md',
+		'typescript',
+		'pages',
+		'blog'
+	);
+	destination = path.join(cwd, projectName, 'pages', 'blog');
+	copy(source, destination, (err, createdFiles) => {
+		if (err) throw err;
+
+		createdFiles.forEach(filePath => console.log(`Created ${filePath}`));
+	});
+
+	// copy post type
+	source = path.join(
+		__dirname,
+		'..',
+		'..',
+		'..',
+		'template',
+		'md',
+		'typescript',
+		'types'
+	);
+	destination = path.join(cwd, projectName, 'types');
+	copy(source, destination, (err, createdFiles) => {
+		if (err) throw err;
+
+		createdFiles.forEach(filePath => console.log(`Created ${filePath}`));
+	});
+
+	// copy utils
+	source = path.join(
+		__dirname,
+		'..',
+		'..',
+		'..',
+		'template',
+		'md',
+		'typescript',
+		'utils'
+	);
+	destination = path.join(cwd, projectName, 'utils');
+	copy(source, destination, (err, createdFiles) => {
+		if (err) throw err;
+
+		createdFiles.forEach(filePath => console.log(`Created ${filePath}`));
+	});
+
+	// copy scripts
+	source = path.join(
+		__dirname,
+		'..',
+		'..',
+		'..',
+		'template',
+		'md',
+		'scripts'
+	);
+	destination = path.join(cwd, projectName, 'scripts');
+	copy(source, destination, (err, createdFiles) => {
+		if (err) throw err;
+
+		createdFiles.forEach(filePath => console.log(`Created ${filePath}`));
+	});
+};

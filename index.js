ab6176fcae65a5990482d82858e293f3bddf06a1
msaaddev <mrsaadirfan@gmail.com>
2022-12-27 19:48:34 +0500

    📦 NEW: First commit

---
+++ index.js
@@ -0,0 +1,23 @@
+#!/usr/bin/env node
+
+/**
+ * create-next-blog
+ * Instantly set up Next.js blog
+ *
+ * @author Saad Irfan <twitter.com/msaaddev>
+ */
+
+const init = require('./utils/init');
+const cli = require('./utils/cli');
+const log = require('./utils/log');
+
+const input = cli.input;
+const flags = cli.flags;
+const { clear, debug } = flags;
+
+(async () => {
+	init({ clear });
+	input.includes(`help`) && cli.showHelp(0);
+
+	debug && log(flags);
+})();

1c3d92aece3bd5976ae9e0ba2c086d665c230396
msaaddev <mrsaadirfan@gmail.com>
2022-12-28 22:45:29 +0500

    👌 IMPROVE: Updated package name

--- utils/init.js
+++ utils/init.js
@@ -5,7 +5,7 @@ const unhandled = require('cli-handle-unhandled');
 module.exports = ({ clear = true }) => {
 	unhandled();
 	welcome({
-		title: `setup-next-blog`,
+		title: `${pkg.name}`,
 		tagLine: `by Saad Irfan`,
 		description: pkg.description,
 		version: pkg.version,

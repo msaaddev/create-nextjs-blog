c712ad42e80ef18a0d273982455e587bc1bf80ef
msaaddev <mrsaadirfan@gmail.com>
2022-12-28 20:03:47 +0500

    👌 IMPROVE: Added dir exist validation

--- utils/ask.js
+++ utils/ask.js
@@ -1,4 +1,7 @@
 const { Input, AutoComplete } = require('enquirer');
+const fs = require('fs');
+const path = require('path');
+const cwd = process.cwd();
 
 /**
  * Create a user for input
@@ -15,7 +18,8 @@ const inputPrompt = ({ name, message, hint }) => {
 		validate(value) {
 			const regex = /\s/;
 			if (regex.test(value)) return 'Project name cannot have spaces.';
-
+			if (fs.existsSync(path.join(cwd, value)))
+				return 'Project name already exists.';
 			return true;
 		}
 	});

20d5508b5e879b6d4edae4188152edce922758a9
msaaddev <mrsaadirfan@gmail.com>
2023-01-07 16:48:24 +0500

    📦 NEW: Added category template files

---
+++ template/md/typescript/pages/category/_index.tsx
@@ -0,0 +1,27 @@
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import Link from 'next/link';
+
+import categories from '../../data/categories.json';
+
+const Category: NextPage = () => {
+	return (
+		<>
+			<Head>
+				<title>Blog Categories</title>
+				<meta name="description" content="Category page" />
+			</Head>
+			<section>
+				{categories.map((category, index) => (
+					<Link href={`/category/${category.name}`} key={index}>
+						<div>
+							<h2>{category.name}</h2>
+						</div>
+					</Link>
+				))}
+			</section>
+		</>
+	);
+};
+
+export default Category;
